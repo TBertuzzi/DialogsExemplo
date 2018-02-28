8a57f371a651480834d43ef067124dbbf7ba879c
Thiago Bertuzzi <thiago.bertuzzi@gmail.com>
2018-02-28 11:58:01 -0300

    Primeira versão

--- DialogsExemplo/DialogsExemplo/MainPage.xaml.cs
+++ DialogsExemplo/DialogsExemplo/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Acr.UserDialogs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,34 @@ namespace DialogsExemplo
 		public MainPage()
 		{
 			InitializeComponent();
-		}
+
+            btnToast.Clicked += (sender, e) => {
+
+                UserDialogs.Instance.Toast("Toast!! Eu entendia HELP no Mortal Kombat", TimeSpan.FromSeconds(10));
+            };
+
+            btnLoading.Clicked += async (sender, e) => {
+
+                using (var Dialog = UserDialogs.Instance.Loading("Uma hora acaba..", null, null, true, MaskType.Black))
+                {
+                    await Task.Delay(3000);
+                }
+            };
+
+            btnActionSheet.Clicked +=  (sender, e) => {
+
+                var ActionSheetConfig = new ActionSheetConfig()
+                    .SetTitle("Este Artigo Foi")
+                    .SetUseBottomSheet(true);
+
+                ActionSheetConfig.Add("Mais ou menos");
+                ActionSheetConfig.Add("Bom");
+                ActionSheetConfig.Add("Otimo");
+                ActionSheetConfig.Add("Darth Vader Vestido de Batman Montado em um Charizard");
+
+
+                UserDialogs.Instance.ActionSheet(ActionSheetConfig);
+            };
+        }
 	}
 }
